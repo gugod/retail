ca86e1ba4ff5e6917517c64cf4cc21edd2e9b6c3
Liu Kang-min <gugod@gugod.org>
2008-09-02 09:34:42 +0900

    Remove comments.

--- lib/Retail/View/Commodity.pm
+++ lib/Retail/View/Commodity.pm
@@ -49,20 +49,10 @@ template 'view' => sub {
 
 };
 
-use Imager;
-
 template "pic" => sub {
     my $id = get("id");
     my $record = Commodity($id);
-
-#     my $img = Imager->new();
-#     $img->read(data => $record->pic);
-#     my $data;
-#     $img->scale(xpixels => 80, ypixels => 80)->write(type => "png", data => \$data);
-
     Jifty->handler->apache->content_type("image/png");
-#    outs_raw($data);
-
     outs_raw($record->pic);
 };
 
