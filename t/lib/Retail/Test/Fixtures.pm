1a04faacec59842830a62749299a4e544fbf67d2
Liu Kang-min <gugod@gugod.org>
2008-09-02 17:18:54 +0900

    Add a fixtures helper module.

---
+++ t/lib/Retail/Test/Fixtures.pm
@@ -0,0 +1,28 @@
+package Retail::Test::Fixtures;
+
+use JiftyX::ModelHelpers;
+
+sub import {
+    load(@_);
+}
+
+# Retail::Test::Fixtures->load(qw(providers commodities))
+
+sub import {
+    my ($class, @fixtures) = @_;
+    for(@fixtures) {
+        if ($class->can($_)) {
+            $class->$_();
+        }
+    }
+}
+
+sub providers {
+    Provider->create(name => "good company");
+}
+
+sub commodities {
+    Commodity->create(name => "Hello Kitty A");
+}
+
+1;
